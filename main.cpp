3f11e0770a67b7ef3919d342ac56947acdc094b1
fengyang182036 <fengyang182036@live.com>
2017-10-26 19:29:26 +0800

    project update for recurve dir.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <QDir>
+#include <iostream>
+
+void recurve()
+{
+    QDir dir;
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting(QDir::Size | QDir::Reversed);
+
+    QFileInfoList list = dir.entryInfoList();
+    std::cout << "     Bytes Filename" << std::endl;
+
+    for (int i = 0; i < list.size(); ++i) {
+        QFileInfo fileInfo = list.at(i);
+        std::cout << qPrintable(QString("%1 %2").arg(fileInfo.size(), 10)
+                                                .arg(fileInfo.fileName()));
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    recurve();
+    return 0;
+}
